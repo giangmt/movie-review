58f1a57388c21fa8d472b92fc824a9af5576d6f8
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-14 17:37:58 +0700

    login with oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,33 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :github]
+
+  class << self
+    def from_omniauth auth
+      auth_provider = auth.provider
+      auth_uid = auth.uid
+      auth_info = auth.info
+
+      where(provider: auth_provider, uid: auth_uid).first_or_create do |user|
+        user.uid = auth_uid
+        user.provider = auth_provider
+        user.email = auth_info.email
+        user.password = Devise.friendly_token[Settings.min_password_length,
+          Settings.max_password_length]
+      end
+    end
+  
+    def new_with_session params, session
+      super.tap do |user|
+        facebook_data = session["devise.facebook_data"]
+        github_data = session["devise.github_data"]
+
+        if data = facebook_data || github_data
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    omniauth_callbacks: "users/omniauth_callbacks"
+   }
 end

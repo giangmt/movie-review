a42dad8a11a6d8686b081f23e42d08544a94241b
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-15 02:14:37 +0700

    movie details

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#home"
 
-  resources :movies , only: :index
+  resources :movies , only: [:index, :show]
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions",

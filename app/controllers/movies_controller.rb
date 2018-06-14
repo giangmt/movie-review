d4557dc3bf221fa94ec41be61783b19c3269e5f7
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-15 02:14:37 +0700

    movie details

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.paginate page: params[:page], per_page: Settings.movies.page
   end
+
+  def show
+    @movie = Movie.find_by id: params[:id]
+    redirect_to root_path unless @movie
+  end
 end

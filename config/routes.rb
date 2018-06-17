07f55611af15e17529a9ce60465912ff6e2ef91f
giangmt <maitruonggiangbkhn@gmail.com>
2018-06-17 13:09:10 +0700

    confrimable devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
-Rails.application.routes.draw do
+  Rails.application.routes.draw do
   root "static_pages#home"
 
   resources :movies , only: [:index, :show] do
- 	  resources :reviews, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   
   resources :interactives, only: :create
@@ -12,4 +12,12 @@ Rails.application.routes.draw do
     sessions: "users/sessions",
     omniauth_callbacks: "users/omniauth_callbacks"
    }
+
+   as :user do
+    get "signin", to: "users/sessions#new"
+    post "signin", to: "users/sessions#create"
+    get "signup", to: "users/registrations#new"
+    post "signup", to: "users/registrations#create"
+    delete "signout", to: "users/sessions#destroy"
+   end
 end
